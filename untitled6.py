1cbc4e6ea76cfdfd12c241ce9bdf3619c1b26385
natemanle <nathanbenignos67@gmail.com>
2025-11-11 13:47:11 +0400

    Update untitled6.py

--- untitled6.py
+++ untitled6.py
@@ -1,13 +1,15 @@
-correct_password = int(input("choose a password: "))
+correct_password="12345"
 
 
 password = int(input("Enter the password: ")) #will ask the user to enter a password
 
 
-while password !=1 correct_password:
+while password != correct_password:
     print("Incorrect password. Please try again.")
     password = input("Enter the password: ") #will keep asking until password is correct
 
 
 print("Access granted. Welcome!") #outputs "Access granted. Welcome!"
+
+
 
